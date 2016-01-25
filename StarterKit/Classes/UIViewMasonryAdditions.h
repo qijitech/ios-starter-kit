6d4a45b5b7c45511faa143e240ea43a6e9831912
杨玉刚 <smartydroid@gmail.com>
2016-01-25 21:10:38 +0800

    add masonry distribute space category

---
+++ StarterKit/Classes/UIViewMasonryAdditions.h
@@ -0,0 +1,14 @@
+//
+// Created by Hammer on 1/25/16.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UIView (Masonry)
+
+- (void)distributeSpacingHorizontallyWith:(NSArray *)views;
+
+- (void)distributeSpacingVerticallyWith:(NSArray *)views;
+
+@end
