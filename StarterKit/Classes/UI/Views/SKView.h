0d1c8f5689d8168f9cf2ba070adb5719328f8111
杨玉刚 <smartydroid@gmail.com>
2016-01-25 22:03:09 +0800

    add base skview

--- StarterKit/Classes/UI/Views/SKView.h
+++ StarterKit/Classes/UI/Views/SKView.h
@@ -3,7 +3,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Overcoat/OVCUtilities.h>
 
+NS_ASSUME_NONNULL_BEGIN
 
-@interface SKView : UIView
+@interface SKView
+OVCGenerics(Model) : UIView
+
+- (void)updateConstraintsInternal;
+
+- (void)setupViews;
+
+- (void)configureWithData:(OVC_NULLABLE OVCGenericType(Model, id))entity;
+
 @end
+
+NS_ASSUME_NONNULL_END
--- StarterKit/Classes/UI/Views/SKView.m
+++ StarterKit/Classes/UI/Views/SKView.m
@@ -4,6 +4,50 @@
 
 #import "SKView.h"
 
+@interface SKView ()
+@property(nonatomic, assign) BOOL didSetupConstraints;
+@end
+
 
 @implementation SKView
+
+- (instancetype)init {
+  if (self = [super init]) {
+    [self setupViews];
+  }
+  return self;
+}
+
+- (void)updateConstraints {
+  if (!self.didSetupConstraints) {
+    self.didSetupConstraints = YES;
+    [self updateConstraintsInternal];
+  }
+  [super updateConstraints];
+}
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+
+  // Make sure the contentView does a layout pass here so that its subviews have their frames set, which we
+  // need to use to set the preferredMaxLayoutWidth below.
+  [self setNeedsLayout];
+  [self layoutIfNeeded];
+}
+
+// Initialization code
+- (void)setupViews {
+  // sub class implement
+}
+
+- (void)updateConstraintsInternal {
+  // sub class implement
+}
+
+- (void)configureWithData:(id)entity {
+  // Make sure the constraints have been added to this cell, since it may have just been created from scratch
+  [self updateConstraintsIfNeeded];
+  [self setNeedsUpdateConstraints];
+}
+
 @end
